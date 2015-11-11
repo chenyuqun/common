e44c6700e883f1963fea7d678598475e0233439e
snowzhang <snowzhang@zizaike.com>
2015-11-11 14:19:31 +0800

    edit

--- zizaike-core/src/main/java/com/zizaike/core/framework/cache/CacheKey.java
+++ zizaike-core/src/main/java/com/zizaike/core/framework/cache/CacheKey.java
@@ -1,12 +1,3 @@
-/**  
- * Project Name:zizaike-core  <br/>
- * File Name:CacheKeyPrefix.java  <br/>
- * Package Name:com.zizaike.core.framework.cache  <br/>
- * Date:2015年11月7日下午5:37:11  <br/>
- * Copyright (c) 2015, zizaike.com All Rights Reserved.  
- *  
-*/  
-  
 package com.zizaike.core.framework.cache;  
 /**  
  * ClassName:CacheKey <br/>  
