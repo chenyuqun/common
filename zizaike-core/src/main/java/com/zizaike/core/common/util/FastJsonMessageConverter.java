9a88ea9a68ede6d936aeb6b93c6b01320d55bccb
snowzhang <snowzhang@zizaike.com>
2016-01-07 14:12:37 +0800

    json 转换

--- zizaike-core/src/main/java/com/zizaike/core/common/util/FastJsonMessageConverter.java
+++ zizaike-core/src/main/java/com/zizaike/core/common/util/FastJsonMessageConverter.java
@@ -1,14 +1,12 @@
 package com.zizaike.core.common.util;
 
-import java.io.UnsupportedEncodingException;
 
 import org.springframework.amqp.core.Message;
 import org.springframework.amqp.core.MessageProperties;
-import org.springframework.amqp.support.converter.AbstractMessageConverter;
+import org.springframework.amqp.support.converter.AbstractJsonMessageConverter;
 import org.springframework.amqp.support.converter.MessageConversionException;
 
 import com.alibaba.fastjson.JSON;
-import com.alibaba.fastjson.serializer.SerializerFeature;
 
 /**
  * 
@@ -20,53 +18,25 @@ import com.alibaba.fastjson.serializer.SerializerFeature;
  * @version   
  * @since JDK 1.7
  */
-public class FastJsonMessageConverter  extends AbstractMessageConverter {
+public class FastJsonMessageConverter  extends AbstractJsonMessageConverter {
 
-    public static final String DEFAULT_CHARSET = "UTF-8";
+    @Override
+    protected Message createMessage(Object object,
+            MessageProperties messageProperties) {
+        messageProperties.setContentType(MessageProperties.CONTENT_TYPE_JSON);
+        messageProperties.setContentEncoding("utf-8");
+        return new Message(JSON.toJSONBytes(object), messageProperties);
+    }
 
-    private volatile String defaultCharset = DEFAULT_CHARSET;
-    
-    public FastJsonMessageConverter() {
-        super();
-    }
-    
-    public void setDefaultCharset(String defaultCharset) {
-        this.defaultCharset = (defaultCharset != null) ? defaultCharset
-                : DEFAULT_CHARSET;
-    }
-    
+    @Override
     public Object fromMessage(Message message)
             throws MessageConversionException {
-        return null;
-    }
-    
-    public <T> T fromMessage(Message message,T t) {
-        String json = "";
-        try {
-            json = new String(message.getBody(),"UTF-8");
-        } catch (UnsupportedEncodingException e) {
-            e.printStackTrace();
+        Object content = message.getBody();
+        if(content == null){
+            return null;
         }
-        return (T) JSON.parseObject(json, t.getClass());
-    }   
-    
+  
 
-    protected Message createMessage(Object objectToConvert,
-            MessageProperties messageProperties)
-            throws MessageConversionException {
-        byte[] bytes = null;
-        try {
-            String jsonString = JSON.toJSONString(objectToConvert,SerializerFeature.WriteMapNullValue);
-            bytes = jsonString.getBytes(this.defaultCharset);
-        } catch (UnsupportedEncodingException e) {
-            throw new MessageConversionException(
-                    "Failed to convert Message content", e);
-        } 
-        messageProperties.setContentType(MessageProperties.CONTENT_TYPE_JSON);
-        messageProperties.setContentEncoding(this.defaultCharset);
-        if (bytes != null) {
-            messageProperties.setContentLength(bytes.length);
-        }
-        return new Message(bytes, messageProperties);
+        return content;
     }
 }
