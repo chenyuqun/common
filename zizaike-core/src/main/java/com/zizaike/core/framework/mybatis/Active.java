a9b60461210df2da091f36dfbb4cf03f07261b3a
snowzhang <snowzhang@zizaike.com>
2016-03-15 12:55:21 +0800

    add shardbatis

---
+++ zizaike-core/src/main/java/com/zizaike/core/framework/mybatis/Active.java
@@ -0,0 +1,24 @@
+/**  
+ * Project Name:zizaike-core  <br/>
+ * File Name:Active.java  <br/>
+ * Package Name:com.zizaike.core.framework.mybatis  <br/>
+ * Date:2016年3月14日下午4:51:21  <br/>
+ * Copyright (c) 2016, zizaike.com All Rights Reserved.  
+ *  
+*/  
+  
+package com.zizaike.core.framework.mybatis;  
+/**  
+ * ClassName:Active <br/>  
+ * Function: 是否可用. <br/>  
+ * Date:     2016年3月14日 下午4:51:21 <br/>  
+ * @author   snow.zhang  
+ * @version    
+ * @since    JDK 1.7  
+ * @see        
+ */
+public class Active {
+    public final static Integer LIVELY = 1;
+    public final static Integer OVER = 0;
+}
+  
