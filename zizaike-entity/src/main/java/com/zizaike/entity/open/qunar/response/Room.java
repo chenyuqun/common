965e15173c8fad5e43f0831fb9a3399ebc715af7
chenyuqun <346573209@qq.com>
2016-05-27 16:16:49 +0800

    增加备注

--- zizaike-entity/src/main/java/com/zizaike/entity/open/qunar/response/Room.java
+++ zizaike-entity/src/main/java/com/zizaike/entity/open/qunar/response/Room.java
@@ -111,7 +111,7 @@ public class Room {
     /**
      * remarks是room的可选节点。一些提示消费者的信息。
      */
-    @XStreamAlias("remarks")
+    //@XStreamAlias("remarks")
     @XStreamImplicit(itemFieldName = "remark")
     private List<Remark> remarks;
 
